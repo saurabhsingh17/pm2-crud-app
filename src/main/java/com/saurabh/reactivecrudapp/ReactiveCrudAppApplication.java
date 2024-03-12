1c829683fc26a6c26fc74fe1147321d2bb618129
saurabhsingh <saurabh623a@gmail.com>
2024-03-12 23:58:48 +0530

    Pushing latest changes

---
+++ src/main/java/com/saurabh/reactivecrudapp/ReactiveCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.reactivecrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCrudAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saurabh/reactivecrudapp/ReactiveCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.reactivecrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
